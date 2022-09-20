c7fde2e5a4f5db7997fd4a776c02fc3333b4b8ab
ComingEvanYou <2045819657@qq.com>
2022-09-20 20:46:29 +0800

    feat(custom): 二次封装axios add api接口封装   add  都能用data进行传参

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import service from "../utils/requset";
+
+const getLogin = () => {
+    return service({
+        url:"",
+        method:"GET",
+        data
+    })
+}
+
+
+export {
+    getLogin
+}
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+
+console.log(process.env.VUE_APP_BASE_API)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
