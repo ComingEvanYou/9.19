51a46921a5b05c126f55a258f51000dd875da2da
ComingEvanYou <2045819657@qq.com>
2022-09-20 16:19:51 +0800

    feat(custom): 配置跨域 add 配置多个跨域代理

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,11 +8,37 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios"
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
-  }
+  },
+  created() {
+    this.getV1()
+    this.getApi()
+  },
+  methods: {
+    getApi(){
+      axios({
+      url:process.env.VUE_APP_BASE_API,
+      methed:"GET"
+    }).then(response=> {
+      console.log("api",response)
+    }).catch(error=> {
+      console.log("api",error)
+    })
+    },
+    getV1(){
+      axios({
+      url:process.env.VUE_APP_BASE_V1,
+      methed:"GET"
+    }).then(response=> {
+      console.log("v1",response)
+    }).catch(error=> {
+      console.log("v1",error)
+    })
+    }
+  },
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer:{
-    port:6888,
-    host:"localhost",
-    open:true,
-    https:false
+  devServer: {
+    port: 6888,
+    host: "localhost",
+    open: true,
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeorigin: true,
+        pathRewrite: {
+          ["^"+process.env.VUE_APP_BASE_API]:""
+        }
+      },
+      [process.env.VUE_APP_BASE_V1]: {
+        target: process.env.VUE_APP_SERVICE_URL_V1,
+        changeorigin: true,
+        pathRewrite: {
+          ["^"+process.env.VUE_APP_BASE_V1]:""
+        }
+      }
+    }
   }
 })
